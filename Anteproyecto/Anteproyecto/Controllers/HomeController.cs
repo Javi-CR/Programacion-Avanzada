7e794086fe824e0f6228a7e8607fd003e6243903
Javi-CR <javierromero2515@gmail.com>
2024-09-28 01:58:41 -0600

    Practica 1-Terminada
    Pendiente: Escoger tema del proyecto y posiblemente cambiar un poco el diseño de la página que tenga sentido con el proyecto

--- Anteproyecto/Anteproyecto/Controllers/HomeController.cs
+++ Anteproyecto/Anteproyecto/Controllers/HomeController.cs
@@ -6,27 +6,28 @@ namespace Anteproyecto.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
+       
         public IActionResult Index()
         {
             return View();
         }
-
-        public IActionResult Privacy()
+        [Route("Login")]
+        public IActionResult Login()
         {
             return View();
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
+        [Route("Register")]
+        public IActionResult Register()
         {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+            return View();
+        }
+
+        [Route("RecuperarCuenta")]
+        public IActionResult RecuperarCuenta()
+        {
+            return View();
         }
     }
+
 }
